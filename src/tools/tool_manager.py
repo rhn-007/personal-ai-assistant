c6c1945df6f29db0883cc3a1ca26f19410c4891c
rhn <rohansibi13@gmail.com>
2026-06-30 11:44:06 +0300

    Update tool_manager.py

--- src/tools/tool_manager.py
+++ src/tools/tool_manager.py
@@ -1,1 +1,24 @@
+class ToolManager:
 
+    def __init__(self):
+        self.tools = []
+
+    def register(self, tool):
+        self.tools.append(tool)
+
+    def get_tool(self, query):
+
+        for tool in self.tools:
+            if tool.can_handle(query):
+                return tool
+
+        return None
+
+    def execute(self, query):
+
+        tool = self.get_tool(query)
+
+        if tool:
+            return tool.execute(query)
+
+        return None
